870f5e4d51002237fd1cde982b3dfdf7f4924efd
zjwhhhhhhhhh <133022249+zjwhhhhhhhhh@users.noreply.github.com>
2024-04-08 23:47:11 +0800

    Update dag2file.go

--- dag2file.go
+++ dag2file.go
@@ -4,61 +4,60 @@ import (
 	"encoding/json"
 	"strings"
 )
+
+// blob，list，tree这几个基本结构的基准都是4字节，所以每四个字节作为基准提取信息
+const STEP = 4
+
 // Hash to file
 func Hash2File(store KVStore, hash []byte, path string, hp HashPool) []byte {
-	// ����hash��path�� ���ض�Ӧ���ļ�, hash��Ӧ��������tree
-	flag,_ := store.Has(hash); //����has�����鿴�洢���Ƿ����ָ���Ķ���
+	// 根据hash和path， 返回currentObjBinary对应的文件, hash对应的类型是tree
+	flag, _ := store.Has(hash)
+	// 判断kvstore里是否拥有hash这个key对应的value，如果有则进行拼装
 	if flag {
-		objBinary,_ := store.Get(hash); //��ȡ����������
-		obj := binaryToObj(objBinary); //��������������
-        pathArr:=strings.Split(path,"\\");//��·���ַ������շָ����ָ������pathArr
+		objBinary, _ := store.Get(hash)
+		obj := binary2Obj(objBinary)
+		pathArr := strings.Split(path, "\\")
 		cur := 1
-		//��������õ��Ķ���·�����顢��ǰ·�������ʹ洢����,�ú��������ָ���ļ��������ļ�����
-		return getFileByDir(obj,pathArr,cur,store)
+		return getFileByDir(obj, pathArr, cur, store)
 	}
 	return nil
 }
 
-func getFileByDir(obj* Object,pathArr []string,cur int,store KVStore) []byte {
-	//�жϵ�ǰ������·�������Ƿ񳬳���·������ĳ��ȣ���˵���Ѿ�������·����ֱ�ӷ��� nil
-	if cur >= len(path) {
+// 拼装tree
+func getFileByDir(obj *Object, pathArr []string, cur int, store KVStore) []byte {
+	if cur >= len(pathArr) {
 		return nil
 	}
 	index := 0
-	//������ǰĿ¼�����е���������
 	for i := range obj.Links {
-		//�� obj.Data �л�ȡ��ǰ���ӵ����ͣ�������ת��Ϊ�ַ�����index �� index+STEP �ǵ�ǰ���������� obj.Data �е�������Χ
 		objType := string(obj.Data[index : index+STEP])
 		index += STEP
-		//��ȡ��ǰ���ӵ���Ϣ
 		objInfo := obj.Links[i]
-		//�����ǰ���ӵ�������·�������е�ǰ������Ӧ��·����ƥ�䣬��������ǰ���ӣ�������һ�ε���
 		if objInfo.Name != pathArr[cur] {
 			continue
 		}
 		switch objType {
 		case TREE:
 			objDirBinary, _ := store.Get(objInfo.Hash)
-			objDir := binaryToObj(objDirBinary)
+			objDir := binary2Obj(objDirBinary)
 			ans := getFileByDir(objDir, pathArr, cur+1, store)
 			if ans != nil {
 				return ans
 			}
-		//�ļ�����
 		case BLOB:
 			ans, _ := store.Get(objInfo.Hash)
 			return ans
-		//�б�����
 		case LIST:
 			objLinkBinary, _ := store.Get(objInfo.Hash)
-			objList := binaryToObj(objLinkBinary)
+			objList := binary2Obj(objLinkBinary)
 			ans := getFileByList(objList, store)
 			return ans
 		}
 	}
 	return nil
 }
-//�б������еݹ�����ļ������������ҵ����ļ�����ƴ�ӳ�һ�����[]byte ��Ƭ����
+
+// 将blob和list拼装为file
 func getFileByList(obj *Object, store KVStore) []byte {
 	ans := make([]byte, 0)
 	index := 0
@@ -67,7 +66,7 @@ func getFileByList(obj *Object, store KVStore) []byte {
 		index += STEP
 		curObjLink := obj.Links[i]
 		curObjBinary, _ := store.Get(curObjLink.Hash)
-		curObj := binaryToObj(curObjBinary)
+		curObj := binary2Obj(curObjBinary)
 		if curObjType == BLOB {
 			ans = append(ans, curObjBinary...)
 		} else { //List
@@ -77,11 +76,10 @@ func getFileByList(obj *Object, store KVStore) []byte {
 	}
 	return ans
 }
-//�����������ݽ����ɶ���
-func binaryToObj(objBinary []byte) *Object {
-	var res Object
-	//ʹ�� json.Unmarshal ���������������� objBinary ������ Object �ṹ�壬���洢�� res ������
-	json.Unmarshal(objBinary, &res)
-	return &res  //���ص�ַ
-}
 
+// 将新的byte数组序列化拼接并返回序列化之后的结果
+func binary2Obj(objBinary []byte) *Object {
+	var res Object
+	json.Unmarshal(objBinary, &res)
+	return &res
+}
